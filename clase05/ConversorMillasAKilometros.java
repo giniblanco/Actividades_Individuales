5cfd6a29813993fc88c69ee37ff3a4ee906703be
giniblanco <blancogini@gmail.com>
2023-06-06 22:56:38 +0100

    first commit

--- clase05/ConversorMillasAKilometros.java
+++ clase05/ConversorMillasAKilometros.java
@@ -7,7 +7,7 @@ public class ConversorMillasAKilometros {
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese la distancia en millas: ");
         int millas = leer.nextInt();
-        Double kilometros = millas * 1.60934;
+        double kilometros = millas * 1.60934;
         System.out.println("La distancia en kilometros es: " + kilometros);
     }
 }
