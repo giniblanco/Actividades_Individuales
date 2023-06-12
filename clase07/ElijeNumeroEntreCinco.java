7e3abb54b79e358d297a3cdcd20acab51a5669d6
giniblanco <blancogini@gmail.com>
2023-06-12 22:47:40 +0100

    cambios

--- clase07/ElijeNumeroEntreCinco.java
+++ clase07/ElijeNumeroEntreCinco.java
@@ -4,17 +4,39 @@ import java.util.Scanner;
 
 public class ElijeNumeroEntreCinco {
     public static void main(String[] args) {
-        System.out.println("Ingrese un numero entre 1 y 10");
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in); //instanciar el teclado
         int[] numeros = new int[5];
 
         int contador = 0;
-        while (contador < 5) {
-            System.out.println("Ingrese un numero");
+        while (contador < numeros.length) {
+            System.out.println("Ingrese un numero: ");
             numeros[contador] = teclado.nextInt();
-            System.out.println(numeros[contador] + " ");
             contador++;
         }
 
+        System.out.println("Los números ingresados son:");
+
+        contador = 0;
+        while (contador < numeros.length) {
+            System.out.print("+--------+");
+            contador++;
+        }
+
+        System.out.println();
+
+        contador = 0;
+        while (contador < numeros.length) {
+            System.out.print("|   " + numeros[contador] + "    ");
+            contador++;
+        }
+
+        System.out.println("|");
+
+        contador = 0;
+        while (contador < numeros.length) {
+            System.out.print("+--------+");
+            contador++;
+        }
     }
-}
+    }
+
