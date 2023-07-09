051b0bc4dc6c1a41299f16ec36077c18eb7b453a
giniblanco <blancogini@gmail.com>
2023-07-09 22:32:37 +0100

    Primer commit

--- clase12/clase11/src/main/java/com/bootcamp/java/clase11/Controllers/UsuarioController.java
+++ clase12/clase11/src/main/java/com/bootcamp/java/clase11/Controllers/UsuarioController.java
@@ -5,24 +5,40 @@ import com.bootcamp.java.clase11.models.Usuario;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    static List<Usuario> usuarios = List.of(new Usuario(1,"Lucas","Moy","123456789", "lucas@moy.com"));
+    // Lista de usuarios
+    List<Usuario> usuarios = new ArrayList<>();
 
+    {
+        // Agregar un usuario de ejemplo a la lista (inicialización)
+        usuarios.add(new Usuario(27, "Lucas", "Moy", "123456789", "lucas@moy.com"));
+    }
+
+    // Obtener todos los usuarios
     @GetMapping("/api/usuarios")
     public List<Usuario> listarUsuarios() {
-
         return usuarios;
 }
 
-    @DeleteMapping("/prueba")
-    public void eliminarUsuario(@PathVariable String id) {
-        usuarios.remove(0);
+
+    // Eliminar un usuario por su ID
+    @DeleteMapping("/api/usuarios/{id}")
+    public void eliminarUsuario(@PathVariable Integer id) {
+        Iterator<Usuario> iterator = usuarios.iterator();
+        while (iterator.hasNext()) {
+            Usuario user = iterator.next();
+            if (id == user.getId()) {
+                iterator.remove();
+            }
+        }
     }
 
+    // Ruta de prueba
     @PostMapping("/prueba")
     public String prueba2() {
         return "Prueba Post";
--- clase12/clase11/src/main/java/com/bootcamp/java/clase11/models/Usuario.java
+++ clase12/clase11/src/main/java/com/bootcamp/java/clase11/models/Usuario.java
@@ -1,9 +1,6 @@
 package com.bootcamp.java.clase11.models;
-
 import lombok.*;
 
-@Getter
-@Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +12,44 @@ public class Usuario {
     private String apellido;
     private String telefono;
     private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
